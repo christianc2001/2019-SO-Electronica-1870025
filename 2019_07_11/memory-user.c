a6285a9b2e3279f6596a412f0124446bdde3978f
christianc2001 <50960470+christianc2001@users.noreply.github.com>
2019-07-11 12:47:01 -0500

    Añadiendo memory-user.c

---
+++ 2019_07_11/memory-user.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (int argc, char **argv){
+
+	if ( argc != 2 ){
+		printf("Por favor, digite un número\n");
+		exit(-1);
+	}
+	int memorysize = atoi(argv[1]);
+	memorysize = memorysize*1024*1024;
+	int *arreglo = (int*) malloc(sizeof(int) * memorysize);
+
+	if (arreglo == NULL) {
+		printf("Error en la asignación de memoria\n");
+		exit(-1);
+	}
+	int i = 0;
+	for (; i < memorysize; i++) {
+		arreglo[i] = i;
+	}
+	sleep(10);
+
+	free(arreglo);
+
+	return 0;
+}
